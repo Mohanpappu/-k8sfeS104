2e6fb7721d36751e936fdbb2610d10436debd519
PAPPU MOHANA KRISHNA <163747615+Mohanpappu@users.noreply.github.com>
2025-11-05 21:53:30 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:30083/back1/auth";
+const API_URL = "http://localhost:30083/back1/api/auth"; // corrected URL
 
-export const login = async (username, password) => {
-  const response = await axios.post(`${API_URL}/login`, { username, password });
-  localStorage.setItem("token", response.data);
+export const signup = async (username, email, password) => {
+  const response = await axios.post(`${API_URL}/signup`, { username, email, password });
   return response.data;
 };
 
-export const signup = async (username, email, password) => {
-  return axios.post(`${API_URL}/signup`, { username, email, password });
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data.token);
+  return response.data;
 };
 
 export const logout = () => {
